6a35d715a38d94ce5794c0dec9e7006d87b1aebd
Edward Mwansa <edwardzmk@gmail.com>
2020-07-26 17:32:34 +0200

    added list Ingredient feature

--- app/core/admin.py
+++ app/core/admin.py
@@ -6,26 +6,27 @@ from django.utils.translation import gettext as _
 
 from core import models
 
+
 class UserAdmin(BaseUserAdmin):
 
     ordering = ['id']
     list_display = ['email', 'name']
 
     fieldsets = (
-    (None, {'fields': ('email', 'password')}),
-    (_('Personal Info'), {'fields': ('name',)}),
-    (
-        _('Permissions'),
-        {
-            'fields': (
-                'is_active',
-                'is_staff',
-                'is_superuser',
-            )
-        }
-    ),
-    (_('Important dates'), {'fields': ('last_login',)}),
-)
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('name',)}),
+        (
+        _   ('Permissions'),
+                {
+                    'fields': (
+                        'is_active',
+                        'is_staff',
+                        'is_superuser',
+                        )
+                }
+            ),
+            (_('Important dates'), {'fields': ('last_login',)}),
+        )
 
     add_fieldsets = (
         (None, {
@@ -36,3 +37,4 @@ class UserAdmin(BaseUserAdmin):
 
 admin.site.register(models.User, UserAdmin)
 admin.site.register(models.Tag)
+admin.site.register(models.Ingredient)
